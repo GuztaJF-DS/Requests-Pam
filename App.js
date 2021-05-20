4d7bc88c432a6409303719e4179263897d48b91e
outro <noobshippuden13@gmail.com>
2021-05-20 15:54:04 -0300

    Added Search Movie Feature

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React,{Component} from 'react'
-import { Text, View, StyleSheet } from 'react-native'
+import { Text, View, StyleSheet,Button,TextInput } from 'react-native'
 import api from './components/api'
 import Movie from './components/Movie'
 
@@ -8,12 +8,21 @@ class App extends Component{
   constructor(props){
     super(props);
     this.state={
-      Movie:[]
+      Movie:[],
+      SetMovie:""
     }
   }
 
   async componentDidMount(){
     const response = await api.get('/?t=Shrek&apikey=539c05e4')
+    this.setState({
+      Movie:response.data
+    })
+    this.loading=this.loading.bind(this)
+  }
+
+  async loading(){
+    const response = await api.get('/?t='+this.state.SetMovie+'&apikey=539c05e4')
     this.setState({
       Movie:response.data
     })
@@ -23,6 +32,13 @@ class App extends Component{
     return (
     <View style={styles.container}>
       <Movie  data={this.state.Movie}/>
+      <TextInput
+        placeholder="Digite o nome do filme"
+        onChangeText={
+          (value)=>this.setState({SetMovie:value})
+        }
+      />
+      <Button title="buscar" onPress={this.loading()}/>
     </View>
   )
   }
