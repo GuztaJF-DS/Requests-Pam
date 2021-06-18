b27e760dc18c027e8a298e1b237435954af22dc9
outro <noobshippuden13@gmail.com>
2021-06-17 21:50:27 -0300

    first commit

--- App.js
+++ App.js
@@ -1,55 +1,178 @@
-import React,{Component} from 'react'
-import { Text, View, StyleSheet,Button,TextInput } from 'react-native'
-import api from './components/api'
-import Movie from './components/Movie'
+import React, { Component } from 'react';
+import { Text, TextInput, Button, View, StyleSheet } from 'react-native';
 
-class App extends Component{
+import Api from './components/Api.js';
 
-  constructor(props){
+const Separator = () => (
+  <View style={styles.separator} />
+);
+
+
+class App extends Component {
+  constructor(props) {
     super(props);
+
     this.state={
-      Movie:[],
-      SetMovie:""
+      nm_user:'',
+      email_user:'',
+      password_user:'',
+      Advice:''
+    }
+    this.Register=this.Register.bind(this)
+    this.Login=this.Login.bind(this)
+    this.Delete=this.Delete.bind(this)
+    this.Change=this.Change.bind(this)
+  }
+
+  async Register(){
+    try{
+      const response= await Api({
+        method:"post",
+        url:"auth/register",
+        data:{
+          nm_user:this.state.nm_user,
+          email_user:this.state.email_user,
+          password_user:this.state.password_user,
+        }
+      })
+      if(response){
+        if(response.data.Error!=null){
+          this.setState({Advice:response.data.Error})
+        }else{
+          this.setState({Advice:response.data.mensage})
+        }
+      }
+    }
+    catch(err){
+        console.log(err);
+        this.setState({Advice:`${err}`})
     }
   }
 
-  async componentDidMount(){
-    const response = await api.get('/?t=Shrek&apikey=539c05e4')
-    this.setState({
-      Movie:response.data
-    })
-    this.loading=this.loading.bind(this)
-  }
-
-  async loading(){
-    const response = await api.get('/?t='+this.state.SetMovie+'&apikey=539c05e4')
-    this.setState({
-      Movie:response.data
-    })
-  }
-
-  render(){
-    return (
-    <View style={styles.container}>
-      <Movie  data={this.state.Movie}/>
-      <TextInput
-        placeholder="Digite o nome do filme"
-        onChangeText={
-          (value)=>this.setState({SetMovie:value})
+  async Login(){
+     try{
+      const response= await Api({
+        method:"post",
+        url:"auth/authenticate",
+        data:{
+          email_user:this.state.email_user,
+          password_user:this.state.password_user,
         }
-      />
-      <Button title="buscar" onPress={this.loading()}/>
-    </View>
-  )
+      })
+      if(response){
+        if(response.data.Error!=null){
+          this.setState({Advice:response.data.Error})
+        }else{
+          this.setState({Advice:response.data.mensage})
+        }
+      }
+    }
+    catch(err){
+        console.log(err);
+    }
+  }
+
+  async Delete(){
+     try{
+      const response= await Api({
+        method:"post",
+        url:"/auth/delete/User",
+        data:{
+          email_user:this.state.email_user,
+          password_user:this.state.password_user,
+        }
+      })
+      if(response){
+        if(response.data.Error!=null){
+          this.setState({Advice:response.data.Error})
+        }else{
+          this.setState({Advice:response.data.mensage})
+        }
+      }
+    }
+    catch(err){
+        console.log(err);
+    }
+  }
+
+  async Change(){
+     try{
+      const response= await Api({
+        method:"put",
+        url:"auth/ConfirmPass",
+        data:{
+          email_user:this.state.email_user,
+          password_user:this.state.password_user,
+        }
+      })
+      if(response){
+        if(response.data.Error!=null){
+          this.setState({Advice:response.data.Error})
+        }else{
+          this.setState({Advice:response.data.mensage})
+        }
+      }
+    }
+    catch(err){
+        console.log(err);
+    }
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.paragraph}>Requisições</Text>
+        <TextInput style={styles.input}
+          placeholder="UserName"
+          value={this.state.nm_user}
+          onChangeText={(value)=>this.setState({nm_user:value})}
+        />
+        <TextInput style={styles.input}
+          placeholder="E-Mail"
+          value={this.state.email_user}
+          onChangeText={(value)=>this.setState({email_user:value})}
+        />
+        <TextInput style={styles.input} 
+          secureTextEntry={true}
+          placeholder="Password" 
+          value={this.state.password_user}
+          onChangeText={(value)=>this.setState({password_user:value})}
+        />
+        <Button style={styles.input} title="Registrar" color="#a6a6a6" onPress={this.Register}/>
+         <Separator />
+        <Button style={styles.input} title="Login" color="#a6a6a6" onPress={this.Login}/>
+        <Separator />
+        <Button style={styles.input} title="Deletar Usuario pelo email e senha" color="#a6a6a6" onPress={this.Delete}/>
+        <Separator />
+        <Button style={styles.input} title="mudar senha" color="#a6a6a6" onPress={this.Change}/>
+        <Text>{this.state.Advice}</Text>
+      </View>
+    );
   }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#ed4f09',
-    alignItems: 'center',
+    backgroundColor: '#ecf0f1',
+    padding: 16,
+  },
+  paragraph: {
+    margin: 24,
+    fontSize: 18,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  input: {
+    fontSize: '17px',
+    marginBottom: '13px',
     justifyContent: 'center',
+    textAlign: 'center',
+    backgroundColor: '#cfd1d1',
+  }, 
+  separator: {
+    marginVertical: 8,
   },
 });
-export default App
+
+export default App;
--- components/api.js
+++ components/api.js
@@ -1,7 +1,12 @@
-import axios from 'axios'
+import axios from 'axios';
 
 const api=axios.create({
-    baseURL: 'https://www.omdbapi.com/',
+  baseURL:'http://localhost:3000/',
+  withCredentials: false,
+  headers: {
+    'Access-Control-Allow-Origin' : '*',
+    'Access-Control-Allow-Methods':'GET,PUT,POST,DELETE,PATCH,OPTIONS',
+  }
 })
 
 export default api;
